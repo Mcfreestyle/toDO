dd334f901d999b0154959065f3c15e665743a2dd
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-09-30 15:18:12 -0500

    feat: app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+import Login from 'views/Login/Login';
+import SignUp from 'views/SignUp/SignUp';
+import Home from 'views/Home/Home';
+import ToDoList from 'views/ToDoList/ToDoList';
+import Completed from 'views/Completed/Completed';
+
+import Layout from 'components/common/Layout';
+
+import {
+  LOGIN,
+  HOME,
+  TO_DO_LIST,
+  COMPLETED,
+  SIGN_UP
+} from 'config/routes/paths';
+import { AuthContextProvider } from 'contexts/authContext';
+import PublicRoute from 'components/router/PublicRoute';
+import PrivateRoute from 'components/router/PrivateRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path={LOGIN} element={<PublicRoute />}>
+            <Route index element={<Login />}></Route>
+            <Route path={SIGN_UP} element={<SignUp />}></Route>
+          </Route>
+          <Route path={HOME} element={<PrivateRoute />}>
+            <Route path={HOME} element={<Layout />}>
+              <Route index element={<Home />}></Route>
+              <Route path={TO_DO_LIST} element={<ToDoList />}></Route>
+              <Route path={COMPLETED} element={<Completed />}></Route>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthContextProvider>
   );
 }
 
